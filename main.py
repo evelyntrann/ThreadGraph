11d108f9dfad9e5b6360e7139b4607bcc4321d74
Uyen Tran <evelyn.t.0108@gmail.com>
2026-01-18 22:30:41 -0600

    Add raw_events post method and test

--- main.py
+++ main.py
@@ -2,8 +2,33 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from db import get_db
 from models import RawEvent
+from sqlalchemy.dialects.postgresql import insert
+from datetime import datetime, timezone
+
+from hashing import compute_content_hash
 
 app = FastAPI()
+
+@app.post("/admin/events")
+def create_events(
+    event: dict, 
+    db: Session = Depends(get_db)
+):
+    content_hash = compute_content_hash(event["payload"])
+    stmt = insert(RawEvent).values(
+        source=event["source"],
+        source_id=event["source_id"],
+        occurred_at=event["occurred_at"],
+        ingested_at=datetime.now(tz=timezone.utc),
+        payload=event["payload"],
+        content_hash=content_hash,
+    ).on_conflict_do_nothing()
+
+    db.execute(stmt)
+    db.commit()
+    return {"status":"created"}
+
+
 
 @app.get("/events/recent")
 def get_recent_events(
